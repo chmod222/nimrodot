ccaac0ce9b55347eac29d872bc1c56a1178d0db8
chmod222 <304922+chmod222@users.noreply.github.com>
2023-06-07 14:23:39 +0200

    utils.`$`: Correctly handle empty strings

--- nodot/utils.nim
+++ nodot/utils.nim
@@ -8,10 +8,11 @@ proc `$`*(str: String): string =
   let strLen = gdInterfacePtr.string_to_utf8_chars(unsafeAddr str, nil, 0)
   result = newString(strLen)
 
-  discard gdInterfacePtr.string_to_utf8_chars(
-    unsafeAddr str,
-    cast[cstring](addr result[0]),
-    strLen)
+  if strLen > 0:
+    discard gdInterfacePtr.string_to_utf8_chars(
+      unsafeAddr str,
+      cast[cstring](addr result[0]),
+      strLen)
 
 proc newString*(native: string): String =
   gdInterfacePtr.string_new_with_utf8_chars(addr result, cstring(native))
